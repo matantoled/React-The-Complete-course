13e25b9aa76fbbea2fb8da3276bea9544be8e570
Matan <matantoled@gmail.com>
2025-06-10 18:42:11 +0300

    adding TabButton

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+export default function TabButton ({children}) {
+    return (
+        <li>
+            <button>
+                {children}
+            </button>
+        </li>
+    )
+}
