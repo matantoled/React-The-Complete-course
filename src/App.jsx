5e6b75adb4c8794c5da61f0c5b836800fe4860ef
Matan <matantoled@gmail.com>
2025-06-12 22:52:07 +0300

    adding data-with-examples and new selectedTopic

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx'
 import TabButton from './components/TabButton.jsx';
 import {useState} from 'react';
+import {EXAMPLES} from './data-with-examples.js'
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectButton) {
     setSelectedTopic(selectButton);
-    console.log(selectedTopic);
   }
   
 
@@ -34,7 +34,13 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
