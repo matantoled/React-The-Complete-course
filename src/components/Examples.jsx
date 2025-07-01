e75abb2da74f82f869dc1840cacf22dc1d0fc87d
Matan <matantoled@gmail.com>
2025-07-01 16:04:10 +0300

    adding Section file

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import { EXAMPLES } from "../data-with-examples.js";
 import TabButton from "../components/TabButton.jsx";
+import Section from "./Section.jsx";
 
 
 export default function Examples() {
@@ -11,8 +12,7 @@ export default function Examples() {
       }
 
     return (
-        <section id="examples">
-                  <h2>Examples</h2>
+        <Section title="Examples" id="examples">
                   <menu>
                     <TabButton isSelected={selectedTopic === "components"} onSelect={() => handleSelect("components")}>Components</TabButton>
                     <TabButton isSelected={selectedTopic === "jsx"} onSelect={() => handleSelect("jsx")}>JSX</TabButton>
@@ -26,6 +26,6 @@ export default function Examples() {
                       <code>{EXAMPLES[selectedTopic].code}</code>
                     </pre>
                   </div> : <h1>Please select a topic.</h1>}
-                </section>
+                </Section>
     )
 }
